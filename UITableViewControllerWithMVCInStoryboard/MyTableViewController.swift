0b26a1394dadb24819fbce4bed105253df4448a0
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-27 20:35:32 +0300

    start

---
+++ UITableViewControllerWithMVCInStoryboard/MyTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MyTableViewController.swift
+//  UITableViewControllerWithMVCInStoryboard
+//
+//  Created by Артем on 5/27/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
